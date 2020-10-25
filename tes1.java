cff4be4ca6c7843537067d27283f0184c2e34cad
joshuasir <jckryder123@gmail.com>
2020-10-25 18:55:42 +0700

    first commit

---
+++ tes1.java
@@ -0,0 +1,8 @@
+public class tes1{
+    public static void main(String[] args){
+        int a = 5;
+        int b = 6;
+        String name="tes";
+        System.out.println(name.trim());
+    }
+}
