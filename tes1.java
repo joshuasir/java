08d2b9b60bc0179023eaae06d38a9f330ead29b8
joshua sirusstara <71873035+joshuasir@users.noreply.github.com>
2020-10-29 14:24:04 +0700

    Delete tes1.java

--- tes1.java
+++
@@ -1,8 +0,0 @@
-public class tes1{
-    public static void main(String[] args){
-        int a = 5;
-        int b = 6;
-        String name="tes";
-        System.out.println(name.trim());
-    }
-}
